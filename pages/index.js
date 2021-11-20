d7f215ee82fb96d4e4c94d45b861a6efdfc85b8a
Nekromenzer <bamaneeshagimshan1999@gmail.com>
2021-11-21 01:39:03 +0530

    add node to map

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home ({posts}) {
       <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
         <div className='lg:col-span-8 col-span-8'>
           {posts.map((post, index) => (
-            <PostCard post={post} key={post.title} />
+            <PostCard post={post.node} key={post.title} />
           ))}
         </div>
 
