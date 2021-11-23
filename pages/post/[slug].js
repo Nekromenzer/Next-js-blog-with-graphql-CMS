98592bf56455da108e9db17378ebe4aab04c7161
Nekromenzer <bamaneeshagimshan1999@gmail.com>
2021-11-23 18:23:21 +0530

    pass props to child comp

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -7,21 +7,28 @@ import {
   Comments,
   CommentsForm
 } from '../../components'
+import { getPostDetails, getPosts } from '../../services'
 // import { getPosts, getPostsEndpoint } from '../services'
 
-const PostDetails = () => {
+const PostDetails = ({ post }) => {
+  {
+    console.log('post', post)
+  }
   return (
     <div className='container mx-auto px-10 mb-8'>
       <div className='grid grid-cols-1 lg:grid-cols-12  gap-12'>
         <div className='col-span-1 lg:col-span-8'>
-          <PostDetail />
-          <Author />
-          <CommentsForm />
-          <Comments />
+          <PostDetail post={post} />
+          <Author author={post.author} />
+          <CommentsForm slug={post.slug} />
+          <Comments slug={post.slug} />
         </div>
         <div className='col-span-1 lg:col-span-4'>
           <div className='relative lg:sticky top-8'>
-            <PostWidget />
+            <PostWidget
+              slug={post.slug}
+              categories={post.categories.map(category => category.slug)}
+            />
             <Categories />
           </div>
         </div>
@@ -31,3 +38,20 @@ const PostDetails = () => {
 }
 
 export default PostDetails
+
+export async function getStaticProps ({ params }) {
+  const data = await getPostDetails(params.slug)
+  return {
+    props: {
+      post: data
+    }
+  }
+}
+
+export async function getStaticPaths () {
+  const posts = await getPosts()
+  return {
+    paths: posts.map(({ node: { slug } }) => ({ params: { slug } })),
+    fallback: true
+  }
+}
