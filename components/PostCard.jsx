dad61701b343bd003c337f93371e6c9a0aeffd59
Nekromenzer <bamaneeshagimshan1999@gmail.com>
2021-11-23 13:43:38 +0530

    post card done

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import moment from 'moment'
 import Link from 'next/link'
+import Image from 'next/image';
+import { FcCalendar } from 'react-icons/fc'
 
 const PostCard = ({ post }) => {
   console.log(post)
@@ -13,11 +15,31 @@ const PostCard = ({ post }) => {
           alt={post.title}
         />
       </div>
-      <h1 className='transaition duration-700 text-center mb-8 cursor-pointer hover:text-pink-600 text-3xl font-semibold'>
-        <Link href={`/post/${post.slug}`}>
-          <a>{post.title}</a>
-        </Link>
+      <h1 className='transition duration-300 text-center mb-8 cursor-pointer hover:text-pink-600 text-3xl font-semibold'>
+        <Link href={`/post/${post.slug}`}>{post.title}</Link>
       </h1>
+      <div className="block lg:flex text-center items-center justify-center mb-8 w-full">
+      <div className="flex items-center justify-center mb-4 lg:mb-0 w-full lg:w-auto mr-8 items-center">
+        <img
+          alt={post.author.name}
+          className="align-middle rounded-full h-7 w-7"
+          src={post.author.photo.url}
+        />
+        <p className="inline align-middle text-gray-700 ml-2 font-medium text-lg">{post.author.name}</p>
+      </div>
+      <div className="font-medium text-gray-700">
+        <FcCalendar className="h-6 w-6 inline mr-2 text-pink-500" />
+        <span className="align-middle">{moment(post.createdAt).format('MMM DD, YYYY')}</span>
+      </div>
+    </div>
+    <p className="text-center text-lg text-gray-700 font-normal px-4 lg:px-20 mb-8">
+      {post.excerpt}
+    </p>
+    <div className="text-center pb-6 lg:pb-4">
+      <Link href={`/post/${post.slug}`}>
+        <span className="transition duration-500 ease transform hover:-translate-y-1 inline-block bg-pink-600 text-lg font-medium rounded-full text-white px-8 py-3 cursor-pointer">Continue Reading</span>
+      </Link>
+    </div>
     </div>
   )
 }
