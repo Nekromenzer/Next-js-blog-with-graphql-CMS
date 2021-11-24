d2b87a9b78349da71464766ebc0637c5849d256a
Nekromenzer <bamaneeshagimshan1999@gmail.com>
2021-11-24 13:23:35 +0530

    bug fix

--- pages/api/comments.js
+++ pages/api/comments.js
@@ -1,4 +1,4 @@
-import { GraphQLClient, gql } from 'graphql'
+import { GraphQLClient, gql } from 'graphql-request'
 
 const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT
 
--- services/index.js
+++ services/index.js
@@ -128,12 +128,14 @@ export const getPostDetails = async slug => {
   return result.post
 }
 
-// export const getPostDetails = async slug => {
-export const submitComment = async obj => {
+export const submitComment = async (obj) => {
   const result = await fetch('/api/comments', {
     method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(obj)
-  })
-  return result.json()
-}
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(obj),
+  });
+
+  return result.json();
+};
