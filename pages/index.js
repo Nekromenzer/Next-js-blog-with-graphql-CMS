8df4c23569abfdda6a422b2c725807dee8516fd2
Nekromenzer <bamaneeshagimshan1999@gmail.com>
2021-11-24 15:07:24 +0530

    featured post comps

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,16 @@
 import Head from 'next/head'
 import { PostCard, Categories, PostWidget } from '../components'
 import { getPosts } from '../services'
+import { FeaturedPosts } from '../sections'
 
-
-export default function Home ({posts}) {
+export default function Home ({ posts }) {
   return (
     <div className='container mx-auto px-10 mb-8'>
       <Head>
         <title>Next Blog</title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
+      <FeaturedPosts />
       <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
         <div className='lg:col-span-8 col-span-8'>
           {posts.map((post, index) => (
